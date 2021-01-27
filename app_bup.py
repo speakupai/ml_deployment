cf7ebd377f8d3a73fd67401a7b2664aa1b4299dc
Rana Ahmad <taimur1871@gmail.com>
2021-01-27 10:09:11 -0700

    change main.py

---
+++ app_bup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26
+"""
+
+
+# 1. Library imports
+import uvicorn
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from Model import IrisModel, IrisSpecies
+
+# 2. Create app and model objects
+app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='style')
+templates = Jinja2Templates(directory="templates")
+
+model = IrisModel()
+
+#3. Welcome page
+
+@app.get('/index', response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse('index.html', {'request':request})
+
+'''@app.get('/predict')
+def read_root():
+    return {'Iris': 'Experiment'}'''
+
+
+# 3. Expose the prediction functionality, make a prediction from the passed
+#    JSON data and return the predicted flower species with the confidence
+@app.post("/")
+def predict_species(iris: IrisSpecies):
+    data = iris.dict()
+    prediction, probability = model.predict_species(
+        data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']
+    )
+    return {
+        'prediction': prediction,
+        'probability': probability
+    }
+
+
+# 4. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
--- main.py
+++ main.py
@@ -1,49 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26
+"""
 
 # 1. Library imports
-import pandas as pd 
-from sklearn.ensemble import RandomForestClassifier
-from pydantic import BaseModel
-import joblib
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+#from Model import IrisModel, IrisSpecies
 
-# 2. Class which describes a single flower measurements
-class IrisSpecies(BaseModel):
-    sepal_length: float 
-    sepal_width: float 
-    petal_length: float 
-    petal_width: float
+# 2. Create app and model objects
+app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='style')
+templates = Jinja2Templates(directory="templates")
 
+#model = IrisModel()
 
-# 3. Class for training the model and making predictions
-class IrisModel:
-    # 6. Class constructor, loads the dataset and loads the model
-    #    if exists. If not, calls the _train_model method and 
-    #    saves the model
-    def __init__(self):
-        self.df = pd.read_csv('iris.csv')
-        self.model_fname_ = 'iris_model.pkl'
-        try:
-            self.model = joblib.load(self.model_fname_)
-        except Exception as _:
-            self.model = self._train_model()
-            joblib.dump(self.model, self.model_fname_)
-        
+#3. Welcome page
 
-    # 4. Perform model training using the RandomForest classifier
-    def _train_model(self):
-        X = self.df.drop('species', axis=1)
-        y = self.df['species']
-        rfc = RandomForestClassifier()
-        model = rfc.fit(X, y)
-        return model
-
-
-    # 5. Make a prediction based on the user-entered data
-    #    Returns the predicted species with its respective probability
-    def predict_species(self, sepal_length, sepal_width, petal_length, petal_width):
-        data_in = [[sepal_length, sepal_width, petal_length, petal_length]]
-        prediction = self.model.predict(data_in)
-        probability = self.model.predict_proba(data_in).max()
-        return prediction[0], probability
+@app.get("{/templates/index}")
+async def read_item(index):
+    return {"index":index}
