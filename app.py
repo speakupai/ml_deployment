cbf3e03ae9b2545108165a159bce82feb562feb8
Rana Ahmad <taimur1871@gmail.com>
2021-01-27 09:25:48 -0700

    add index file

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ Created on Tue Jan 26
 
 # 1. Library imports
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from Model import IrisModel, IrisSpecies
 
 # 2. Create app and model objects
 app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='style')
+templates = Jinja2Templates(directory="templates")
+
 model = IrisModel()
 
 #3. Welcome page
