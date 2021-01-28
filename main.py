ee2494086ca2d63bbdedf70b145d71bebfd19f09
Rana Ahmad <taimur1871@gmail.com>
2021-01-27 19:29:39 -0700

    improved welcome page

--- main.py
+++ main.py
@@ -10,12 +10,6 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from enum import Enum
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
 
 # 2. Create app and model objects
 app = FastAPI()
@@ -24,25 +18,7 @@ templates = Jinja2Templates(directory="templates/")
 
 #3. Welcome page
 
-@app.get("/")
-async def read_root():
-    return {"welcome.html":"h1"}
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
-
-@app.get("/get_page", response_class=HTMLResponse)
-async def get_page(request:Request):
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request:Request):
     return templates.TemplateResponse("welcome.html", {"request": request, "message":"message"})
 
