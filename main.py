d7ec95fdd82762be13bfca1b81887f8494c27726
Rana Ahmad <taimur1871@gmail.com>
2021-01-27 21:04:05 -0700

    file upload button

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ Created on Tue Jan 26
 
 # 1. Library imports
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import shutil
 
 # 2. Create app and model objects
 app = FastAPI()
@@ -22,3 +23,12 @@ templates = Jinja2Templates(directory="templates/")
 async def read_root(request:Request):
     return templates.TemplateResponse("welcome.html", {"request": request, "message":"message"})
 
+@app.post("/files/")
+async def create_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    with open("destination.png", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"filename": file.filename}
