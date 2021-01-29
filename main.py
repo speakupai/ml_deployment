fc34005d62036db0dc501089579bb6e973dec2a4
Rana Ahmad <taimur1871@gmail.com>
2021-01-29 14:09:29 -0700

    added image rendering

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Created on Tue Jan 26
 # 1. Library imports
 import uvicorn
 from fastapi import FastAPI, Request, File, UploadFile
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import shutil, os
@@ -29,13 +29,18 @@ async def read_root(request:Request):
     return templates.TemplateResponse("welcome.html", {"request": request, "message":"message"})
 
 @app.post("/uploads")
-async def create_upload_file(file: UploadFile = File(...)):
+async def create_upload_file( request:Request, file: UploadFile = File(...)):
 
     tmp_uploads_path = './uploads/'
+
+    if not os.path.exists(tmp_uploads_path):
+        os.makedirs(tmp_uploads_path)
+
     p = Path(tmp_uploads_path + file.filename)
     save_uploaded_file(file, p)
 
-    return {"filename": file.filename}
+    return templates.TemplateResponse("upload_page.html", {"request": request, "filename": file.filename}
+    ), FileResponse('../uploades/{}', file.filename)
 
 
 def save_uploaded_file(upload_file: UploadFile, destination: Path) -> None:
@@ -45,35 +50,3 @@ def save_uploaded_file(upload_file: UploadFile, destination: Path) -> None:
     finally:
         upload_file.file.close()
 
-'''
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    tmp_uploads_path = './uploads/'
-
-    if not os.path.exists(tmp_uploads_path):
-        os.makedirs(tmp_uploads_path)
-
-    p = Path(tmp_uploads_path + file)
-    print(p)
-
-    save_uploaded_file(file, p)
-    return {"filename": file.filename}
-
-@app.post("/uploadfiles/")
-async def create_upload_files(files: List[UploadFile] = File(...)):
-    filenames = {"filenames": []}
-
-    tmp_uploads_path = "./uploads/{0}/".format(uuid.uuid1())
-    if not os.path.exists(tmp_uploads_path):
-        os.makedirs(tmp_uploads_path)
-    
-    for file in files:
-        filenames["filenames"].append(file.filename)
-        p = Path(tmp_uploads_path + file.filename)
-        print(p)
-
-        save_uploaded_file(file, p)
-
-    return filenames
-'''
