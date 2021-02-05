5bfc460aeee19d7cf8d3993b96e184ea74aa6639
Rana Ahmad <taimur1871@gmail.com>
2021-02-04 19:47:21 -0700

    add spectrogram stream function

---
+++ utils/spectrogram_stream.py
@@ -0,0 +1,24 @@
+# python3
+
+''' create librosa spectorgram '''
+
+import librosa
+from librosa.feature import melspectrogram
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def create_spectrogram(audio_patch, sr):
+   spect = melspectrogram(y=audio_patch, sr=sr)
+   fig, ax = plt.subplots()
+   S_dB = librosa.power_to_db(spect, ref=np.max)
+   img = librosa.display.specshow(S_dB, x_axis='time',
+                        y_axis='mel', sr=sr,
+                        fmax=8000, ax=ax)
+   fig.colorbar(img, ax=ax, format='%+2.0f dB')
+   ax.set(title='Mel-frequency spectrogram')
+   save_path = './static/spect.png'
+   plt.savefig(save_path)
+
+   return save_path
