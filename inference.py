09d126d6f02db07191d79283433fc74f3ffc5ade
Rana Ahmad <taimur1871@gmail.com>
2021-02-15 11:38:26 -0700

    fixing spectrgram function

--- inference.py
+++ inference.py
@@ -4,9 +4,9 @@ import librosa
 import numpy as np
 import soundfile as sf
 import torch
-from torch.cuda.amp import autocast
+from tqdm import tqdm
 
-from utils import core, data
+from utils import data, spectrogram, spectrogram_clean
 from models.hifi_gan import Generator
 from models.wavenet import WaveNet
 
@@ -15,8 +15,8 @@ from utils.hparams import hparams as hp
 
 def inference(audio_clip):
     original_file = audio_clip
-    save_dir = '/home/taimur/Documents/Online Courses/Fourth Brain/Projects/Audio_super_res/ml_deployment/uploads'
-    checkpoint_path = '/home/taimur/Documents/Online Courses/Fourth Brain/Projects/Audio_super_res/ml_deployment/saved_model/latest_checkpoint.pt'
+    save_dir = './uploaded'
+    checkpoint_path = './saved_model/latest_checkpoint.pt'
     #default_inf_device = 'cpu',
 
     # Load checkpoint
@@ -28,28 +28,31 @@ def inference(audio_clip):
     model.load_state_dict(checkpoint['generator_state_dict'])
     model.eval()
 
-    if os.path.isdir(original_file):
-        inference_files = core.dir_walk(original_file, ('.wav', '.mp3', '.ogg'))
-    elif os.path.isfile(original_file):
-        inference_files = [original_file]
-    else:
-        raise Exception('input must be .wav file or dir containing audio files.')
+    inference_files = [original_file]
 
     with torch.no_grad():
         for file in inference_files:
             filename = os.path.splitext(os.path.split(file)[1])[0]
-            x, _ = librosa.load(file, sr=hp.dsp.sample_rate, mono=True)
+            x, _ = librosa.load(file, sr=16000, mono=True)
             target_length = len(x)
             x = torch.tensor(x).to('cpu')
             x = data.preprocess_inference_data(x,
                                             hp.inference.batched,
                                             hp.inference.batch_size,
                                             hp.inference.sequence_length,
-                                            hp.dsp.sample_rate)
+                                            16000)
 
-            with autocast(enabled=hp.training.mixed_precision):
-                y = [model.inference(x_batch) for x_batch in x]
-            y = data.postprocess_inference_data(y, hp.inference.batched, hp.dsp.sample_rate)
+            # run inference and create spectrograms
+            y = []
+            for x_batch in tqdm(x):
+                spect_orig = np.array(x_batch[0])
+                spectrogram.create_spectrogram(spect_orig)
+                clean_temp = model.inference(x_batch)
+                print(clean_temp)
+                y.append(clean_temp)
+                spectrogram_clean.create_spectrogram(np.array(clean_temp))
+            y = data.postprocess_inference_data(y, hp.inference.batched,
+                                                16000)
             y = y[:target_length].detach().cpu().numpy()
-            sf.write(os.path.join(save_dir, f'{filename}_denoised.wav'), y.astype(np.float32),
-                samplerate=hp.dsp.sample_rate)
+            sf.write(os.path.join(save_dir, f'{filename}_denoised.wav'),
+                    y.astype(np.float32), samplerate=hp.dsp.sample_rate)
--- utils/spectrogram_clean.py
+++ utils/spectrogram_clean.py
@@ -21,6 +21,6 @@ def create_spectrogram(audio_file):
                         fmax=8000, ax=ax)
    fig.colorbar(img, ax=ax, format='%+2.0f dB')
    ax.set(title='Mel-frequency spectrogram')
-   save_path = './uploaded/spect_orig.png'
+   save_path = './uploaded/spect_clean.png'
 
    plt.savefig(save_path)
